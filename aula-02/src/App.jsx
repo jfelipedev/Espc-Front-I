c599f3d43b74b0537d5d324314c9bc8970a35d56
Felipe <joandersonfsbarbosa@gmail.com>
2023-05-10 14:46:15 -0300

    aula 2 atualizada

--- aula-02/src/App.jsx
+++ aula-02/src/App.jsx
@@ -1,16 +1,5 @@
-function App() {
-  function handleCheck() {
-    alert("ok");
-  }
+import RoutesConfig from "./routes";
 
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <input placeholder="Digite um valor" />
-      <button onClick={handleCheck}>verificar</button>
-    </div>
-  );
+export default function App() {
+  return <RoutesConfig />;
 }
-
-export default App;
-
