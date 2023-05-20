39430e2a75232baf51b76ebb626a16b2bd7a61a3
Joanderson Felipe <joandersonfsbarbosa@gmail.com>
2023-05-20 11:23:21 -0300

    funcao de adicionar tarefa em andamento

--- aula-04-ex/src/requests/todos.js
+++ aula-04-ex/src/requests/todos.js
@@ -4,10 +4,6 @@ export async function getTodos(){
     return (await api.get("/todo")).data
 }
 
-// /**
-//  * Função para adicionar nova tarefa
-//  * @param {{title:string, date: Date}} todo 
-//  */
 export async function addTodo(todo){
     return (await api.post("/todo", todo)).data
 }
